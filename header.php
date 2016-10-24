e4c5bf137659b318136e359f509064127d48e51f
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-10-24 19:25:44 -0400

    Add Bootstrap files

--- header.php
+++ header.php
@@ -8,8 +8,18 @@
     <link rel="stylesheet" type="text/css" href="
         <?php bloginfo('stylesheet_url'); ?>
     " />
+
+	<!-- Playfair Display Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+	<!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
 </head>
 <body>
     <div class="container"> <!-- Begin Container Div -->
